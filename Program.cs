4b0af736014479ea02579d888cfb7e615ba0dd06
Gabriela Dyworová <gabriela.dyworova@gmail.com>
2022-11-08 10:55:56 +0100

    Přidejte soubory projektu.

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Zadejte poloměr kruhu:");
+int a = int.Parse(Console.ReadLine());
+
+obvod = 2 * 3.1415 * a;
+obsah = 3.1415 * Math.Pow(a,2);
+
+Console.WriteLine("Obvod kruhu je:" + obvod);
+Console.WriteLine("Obsah kruhu je:" + obsah);
+Console.ReadKey();
